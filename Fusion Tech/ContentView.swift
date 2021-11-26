ab04a19716cf8ece5b9298ed09efd73798064f41
fusion-tech-alexey-tsutsoev <alexey.tsutsoev@fusion-tech.pro>
2021-11-27 02:32:28 +0300

    Initial Commit

---
+++ Fusion Tech/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fusion Tech
+//
+//  Created by Fusion Tech on 27.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fusion Tech/Fusion_TechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fusion_TechApp.swift
+//  Fusion Tech
+//
+//  Created by Fusion Tech on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Fusion_TechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
